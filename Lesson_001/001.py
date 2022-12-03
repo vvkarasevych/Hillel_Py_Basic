ce00447ac0ddc574beac11bdeb0be4e1d53a5983
vvkarasevych <medicus.amicus.it@gmail.com>
2022-12-03 10:08:23 +0100

    First Lesson

--- Lesson_001/001.py
+++ Lesson_001/001.py
@@ -1,7 +1,36 @@
 #  First lesson about print and input
-print('Hello!')
-print('What is your name?')
-name = input()
-print(f'Hello {name} !\nHave a nice day!')
+print('Hello World!')
+print("Hello World!")
+
+#  комментарий к коду
+#  Run - режим запуска
+#  Debug - режим отладки
+
+#  input - ввод
+#  = - оператор присвоения. Правая часть высчитывается, после чего записывается в левую часть
+#  поэтому в левой части всегда ожидается переменная
+name = input('Как вас зовут? ')
+print(name)
+print('Hello', name)
+print(f"Hello, {name}!")
+
+name = 'Another name'
+print('Hello', name)
+print(f"Hello, {name}!")
+
+#  динамическая типизация
+name = 5
+print('Hello', name)
+print(f"Hello, {name}!")
+
+#  integer (int) - тип данных, целые числа
+x = int()  # значение по умолчанию - 0
+print(x)
+x = 0
+print(x)
+
+#  VSC - система контроля версий
+#  позволяет хранить код и все изменения в истории этого кода
+#  обычно один проект хранится в репозитории
 
 #  Proof of my installed and setup system to start the educational course
