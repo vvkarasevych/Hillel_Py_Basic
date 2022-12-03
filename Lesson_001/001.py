cffe5b085aebb48137e7ad3210abbc11cf58d54a
vvkarasevych <medicus.amicus.it@gmail.com>
2022-12-03 09:42:30 +0100

    First Lesson

---
+++ Lesson_001/001.py
@@ -0,0 +1,7 @@
+#  First lesson about print and input
+print('Hello!')
+print('What is your name?')
+name = input()
+print(f'Hello {name} !\nHave a nice day!')
+
+#  Proof of my installed and setup system to start the educational course
